721f7572e0f7b059ad898c4b3dbacf0a294eef9d
pierakim <pierakim.lambda@gmail.com>
2021-03-18 18:19:02 +1000

    edit your data WIP

--- lib/Widgets/MyCollection/MyCollectionComponent.dart
+++ lib/Widgets/MyCollection/MyCollectionComponent.dart
@@ -124,6 +124,7 @@ class _MyCollectionComponentState extends State<MyCollectionComponent> {
       scrollDirection: Axis.vertical,
       child: FittedBox(
         child: DataTable(
+          showCheckboxColumn: false,
           // columnSpacing: 20,
           dataRowHeight: 75,
           dividerThickness: 1,
@@ -144,78 +145,97 @@ class _MyCollectionComponentState extends State<MyCollectionComponent> {
           ],
           rows: paymentCalculatorResults
               .map(
-                (paymentCalculatorResult) => DataRow(cells: [
-                  // FAVOURITE
-                  DataCell(GestureDetector(
-                      onTap: () {
-                        favouriteSelected(paymentCalculatorResult);
-                      },
-                      child: paymentCalculatorResult.isFavourite == 0
-                          ? Icon(Icons.favorite_border_outlined, size: 30)
-                          : Icon(
-                              Icons.favorite,
-                              color: Colors.red,
-                              size: 30,
-                            ))),
-                  // EDIT
-                  DataCell(GestureDetector(
-                      onTap: () {
-                        //editSelected(paymentCalculatorResult.id);
-                      },
-                      child: Icon(Icons.edit_outlined, size: 30))),
-                  // DESCRIPTION/TITLE
-                  DataCell(
-                    Container(
-                      width: 300,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: Container(
-                              // width: 150,
-                              child: Text(paymentCalculatorResult.title,
-                                  style: new TextStyle(
-                                    fontSize: 20.0,
-                                  ),
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis),
-                            ),
+                (paymentCalculatorResult) => DataRow(
+                    onSelectChanged: (bool selected) {
+                      if (selected) {
+                        print(
+                            'row id: ' + paymentCalculatorResult.id.toString());
+                        Navigator.pushNamed(
+                          context,
+                          '/extractArguments',
+                          arguments: new PaymentCalculatorResult(
+                              paymentCalculatorResult.id,
+                              paymentCalculatorResult.title,
+                              paymentCalculatorResult.value01,
+                              paymentCalculatorResult.value02,
+                              paymentCalculatorResult.isFavourite,
+                              paymentCalculatorResult.creationDate,
+                              paymentCalculatorResult.modificationDate),
+                        );
+                      }
+                    },
+                    cells: [
+                      // FAVOURITE
+                      DataCell(GestureDetector(
+                          onTap: () {
+                            favouriteSelected(paymentCalculatorResult);
+                          },
+                          child: paymentCalculatorResult.isFavourite == 0
+                              ? Icon(Icons.favorite_border_outlined, size: 30)
+                              : Icon(
+                                  Icons.favorite,
+                                  color: Colors.red,
+                                  size: 30,
+                                ))),
+                      // EDIT
+                      DataCell(GestureDetector(
+                          onTap: () {
+                            //editSelected(paymentCalculatorResult.id);
+                          },
+                          child: Icon(Icons.edit_outlined, size: 30))),
+                      // DESCRIPTION/TITLE
+                      DataCell(
+                        Container(
+                          width: 300,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Container(
+                                  // width: 150,
+                                  child: Text(paymentCalculatorResult.title,
+                                      style: new TextStyle(
+                                        fontSize: 20.0,
+                                      ),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Container(
+                                  // width: 150,
+                                  child: Text(
+                                      "Value 01: " +
+                                          paymentCalculatorResult.value01
+                                              .toString() +
+                                          "  " +
+                                          "Value 02: " +
+                                          paymentCalculatorResult.value02
+                                              .toString(),
+                                      style: new TextStyle(
+                                        fontSize: 15.0,
+                                      ),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis),
+                                ),
+                              ),
+                            ],
                           ),
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: Container(
-                              // width: 150,
-                              child: Text(
-                                  "Value 01: " +
-                                      paymentCalculatorResult.value01
-                                          .toString() +
-                                      "  " +
-                                      "Value 02: " +
-                                      paymentCalculatorResult.value02
-                                          .toString(),
-                                  style: new TextStyle(
-                                    fontSize: 15.0,
-                                  ),
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis),
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
-                  ),
-                  // DELETE
-                  DataCell(GestureDetector(
-                      onTap: () {
-                        deleteSelected(paymentCalculatorResult.id);
-                      },
-                      child: Icon(
-                        Icons.delete_outlined,
-                        size: 30,
-                      ))),
-                ]),
+                      // DELETE
+                      DataCell(GestureDetector(
+                          onTap: () {
+                            deleteSelected(paymentCalculatorResult.id);
+                          },
+                          child: Icon(
+                            Icons.delete_outlined,
+                            size: 30,
+                          ))),
+                    ]),
               )
               .toList(),
         ),
